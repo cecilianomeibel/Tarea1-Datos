6dda6bc41152fb52378a6edd1c1f5c51b457402e
cecilianomeibel <85046833+cecilianomeibel@users.noreply.github.com>
2021-08-29 21:28:37 +0200

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -12,7 +12,6 @@ public class Cliente {
     public static void main (String [] args) {
         
         ventanacliente ventana1 =new ventanacliente(); // crear un objeto
-        
         ventana1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // para que al cerrar la ventana no siga ejecutandose el programa
 
     }
@@ -20,10 +19,11 @@ public class Cliente {
 
 class ventanacliente extends JFrame {  //La clase hereda de JFrame para poder crear cuadros(ventanas)
     public ventanacliente() { //Constructor
-        setBounds(500,200,300,300); //define ubicacion en x, y , ancho, alto del cuadro
+        setBounds(500,200,600,300); //define ubicacion en x, y , ancho, alto del cuadro
         setResizable(false); //Evita que se puede redimencionar la ventana 
         setTitle("Cliente"); //Colocar un titulo en la ventana 
         Lamina lamina1 = new Lamina(); //Se crea un objeto
+        
         add(lamina1); 
         setVisible(true); //metodo para poder mostrar la ventana
     }
@@ -31,19 +31,26 @@ class ventanacliente extends JFrame {  //La clase hereda de JFrame para poder cr
 
 class Lamina extends JPanel{
     public Lamina(){ //Constructor
-        nick = new JTextField(5);
-        add(nick);
+        
 
-        JLabel texto= new JLabel("CLIENTE 1"); // instancia para agregar una etiqueta de text
-        
-        add(texto); // se añade el texto a la lamina
-        cuadrodetexto = new JTextField(10); //Nos abre un campo para agregar texto
-        add(cuadrodetexto);
-        chat = new JTextArea(12,20); //anota las coordenadas donde lo va a colocar
+        JLabel datos = new JLabel("Datos a ingresar del producto: "); // instancia para agregar una etiqueta de texto
+        add(datos); // se añade el texto a la lamina
+        JLabel valor = new JLabel(" Valor"); // instancia para agregar una etiqueta de texto
+        add(valor); // se añade el texto a la lamina
+        valor_producto = new JTextField(5); //Nos abre un campo para agregar texto
+        add(valor_producto);
+        JLabel peso = new JLabel(" Peso"); // instancia para agregar una etiqueta de texto
+        add(peso); // se añade el texto a la lamina
+        peso_producto = new JTextField(5); //Nos abre un campo para agregar texto
+        add(peso_producto);
+        JLabel impuestos = new JLabel(" (%) Impuestos"); // instancia para agregar una etiqueta de texto
+        add(impuestos); // se añade el texto a la lamina
+        porcentaje_impuestos = new JTextField(5); //Nos abre un campo para agregar texto
+        add(porcentaje_impuestos);
+        chat = new JTextArea(13,25); //anota las coordenadas donde lo va a colocar
         add(chat);
-        
+
         boton1 = new JButton("Enviar"); // instancia para crear un boton 
-        
         ObtenerTexto click = new ObtenerTexto(); //instancia que servira para obtener el texto al presionar el boton
         boton1.addActionListener(click);
         add(boton1); //se añade el boton a la lamina 
@@ -52,14 +59,21 @@ class Lamina extends JPanel{
     private class ObtenerTexto implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-           // System.out.println(cuadrodetexto.getText());
+           
            try {
             Socket socket1 = new Socket ("192.168.0.15" , 8888); //coloca como parametros direccion ip y puerto
+            Envio datos = new Envio(); //encapsular los datos
+            datos.SetValor_producto(valor_producto.getText()); //obtener con lo que agrega en el cuadro de texto
+            datos.SetPeso_producto(peso_producto.getText());
+            datos.SetPorcentaje_impuestos(porcentaje_impuestos.getText());
+
            
             //Crear flujo de datos de salida
             DataOutputStream flujo_salida = new DataOutputStream(socket1.getOutputStream());
             //Con la linea anterior se especifica que el flujo de datos de salida del cliente al servidor se va hacer a traves del socket1
-            flujo_salida.writeUTF(cuadrodetexto.getText()); //Escribe en el flujo de salida lo que hay en el cuadro de texto
+            flujo_salida.writeUTF(valor_producto.getText()); //Escribe en el flujo de salida lo que hay en el cuadro de texto
+            flujo_salida.writeUTF(peso_producto.getText());
+            flujo_salida.writeUTF(porcentaje_impuestos.getText());
             flujo_salida.close(); 
 
 
@@ -76,12 +90,36 @@ class Lamina extends JPanel{
         }
 
     }
-    private JTextField cuadrodetexto, nick;
+    private JTextField valor_producto,peso_producto , porcentaje_impuestos;
     private JTextArea chat;
     private JButton boton1;
 } 
 
+    class Envio{
+        private String valor_producto, peso_producto , porcentaje_impuestos;
+        public String getvalor_producto() {
+            return valor_producto;
+        }
+        public void SetValor_producto(String valor_producto) {
+            this.valor_producto= valor_producto;
+        }
+
+        public String getpeso_producto() {
+            return peso_producto;
+        }
+        public void SetPeso_producto(String peso_producto) {
+            this.peso_producto = peso_producto;
+        }
+
+        public String getporcetaje_impuestos() {
+            return porcentaje_impuestos;
+        }
+        public void SetPorcentaje_impuestos(String porcentaje_impuestos) {
+            this.porcentaje_impuestos= porcentaje_impuestos;
+        }
+
     
+    }
 
  
 
